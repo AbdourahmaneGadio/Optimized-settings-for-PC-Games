af5969111699f30bf3cb555f50a51fdc3e493ef4
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2023-09-07 21:45:46 +0200

    :grey_exclamation: Update of state management

--- components/TableDetailsJeu.js
+++ components/TableDetailsJeu.js
@@ -1,33 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { StyleSheet, View } from "react-native";
 import { Table, TableWrapper, Col } from "react-native-reanimated-table";
 
 export default function TableDetailsJeu({ gameSelected }) {
-
-    const [gameData, setGameData] = useState([]);
-
-     fetch("/gamesData/" + { gameSelected })
-       .then((resp) => resp.json())
-       .then(({ results }) => {
-         results === undefined ? setGameData([]) : setGameData(results);
-       })
-       .catch(() => {
-         alert("Something happened when fetching game's settings. Try later...");
-       });
-    
-  // On récupère les données du jeu
-
-  let titlesGameData = getKeysFromJSON(gameData);
-  let detailsGameData = getValuesFromJSON(gameData);
+  const [gameData, setGameData] = useState([]);
+  const [titlesGameData, setTitlesGameData] = useState([]);
+  const [detailsGameData, setDetailsGameData] = useState([]);
 
   // Get all the settings name (ex: Texture, Ray-Tracing, ...)
   function getKeysFromJSON(gamesData) {
     let keysResults = [];
 
-    gamesData && Object.keys(gamesData).map(function (key) {
+    Object.keys(gamesData).map(function (key) {
       keysResults.push(key);
     });
-
     return keysResults;
   }
 
@@ -35,42 +21,64 @@ export default function TableDetailsJeu({ gameSelected }) {
   function getValuesFromJSON(gamesData) {
     let valuesResults = [];
 
-     gamesData &&
     Object.values(gamesData).map(function (key) {
-         valuesResults.push(key);
-       });
+      valuesResults.push(key);
+    });
 
     return valuesResults;
   }
 
+  useEffect(() => {
+    async function fetchData() {
+      fetch(
+        `https://raw.githubusercontent.com/AbdourahmaneGadio/Optimized-settings-for-PC-Games/master/gamesData/${gameSelected}.json`
+      )
+        .then((resp) => resp.json())
+        .then((data) => {
+          const gameDataFetch = data !== undefined ? data : [];
+          setGameData(gameDataFetch);
+        });
+    }
+    fetchData();
+
+  }, [setGameData]);
+
+
+ useEffect(() => {
+   async function setGamesTableData() {
+     const keysJSON = getKeysFromJSON(gameData);
+     setTitlesGameData(keysJSON);
+
+     const valuesJSON = getValuesFromJSON(gameData);
+     setDetailsGameData(valuesJSON);
+   }
+
+   setGamesTableData();
+ }, [setTitlesGameData]);
+
+  console.log(detailsGameData);
+
   return (
     <View>
-      {gameData && gameData.length > 0 ? (
-        <Table
-          style={{ flexDirection: "row" }}
-          borderStyle={{ borderWidth: 1 }}
-        >
-          <TableWrapper style={{ width: 500 }}>
-            <TableWrapper style={{ flexDirection: "row" }}>
-              <Col
-                data={titlesGameData}
-                style={styles.title}
-                heightArr={[30, 30, 30, 30]}
-                textStyle={styles.titleText}
-              />
+      <Table style={{ flexDirection: "row" }} borderStyle={{ borderWidth: 1 }}>
+        <TableWrapper style={{ width: 500 }}>
+          <TableWrapper style={{ flexDirection: "row" }}>
+            <Col
+              data={["test", "test"]}
+              style={styles.title}
+              heightArr={[30, 30, 30, 30]}
+              textStyle={styles.titleText}
+            />
 
-              <Col
-                data={detailsGameData}
-                style={styles.title}
-                heightArr={[30, 30, 30, 30]}
-                textStyle={styles.titleText}
-              />
-            </TableWrapper>
+            <Col
+              data={["test", "test"]}
+              style={styles.title}
+              heightArr={[30, 30, 30, 30]}
+              textStyle={styles.titleText}
+            />
           </TableWrapper>
-        </Table>
-      ) : (
-        <Text>Game's settings for this game doesn't exist yet.</Text>
-      )}
+        </TableWrapper>
+      </Table>
     </View>
   );
 }
