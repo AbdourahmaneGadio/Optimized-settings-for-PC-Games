6b9366897c0166594757f8873f1c46f358adc814
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2023-09-08 11:23:46 +0200

    :new: Change results borderRadius

--- components/Results.js
+++ components/Results.js
@@ -1,7 +1,32 @@
-import React from "react";
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { RAWG_API_KEY } from "@env";
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { ImageBackground } from "react-native-web";
 
 const Results = ({ gameResults, onPress }) => {
+  const [video, setVideo] = useState("");
+
+  const myButton = (
+    <Icon
+      name="play"
+      style={styles.playButton}
+      onPress={() => console.log('video press')}
+    />
+  );
+
+  const getVideo = async () => {
+    await fetch(`https://rawg.io/api/games/3498/movies?key=${RAWG_API_KEY}`)
+      .then((resp) => resp.json())
+      .then(({ results }) => {
+        const video = results[0].data["480"];
+        setVideo(video);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <View>
       <Text>Results : </Text>
@@ -14,11 +39,14 @@ const Results = ({ gameResults, onPress }) => {
                 <View style={styles.containerTitle}>
                   <Text>{actualGame.name}</Text>
                 </View>
-                <View>
-                  <Image
+                <View style={styles.imageContainer}>
+                  <ImageBackground
                     source={actualGame.background_image}
                     style={styles.imageCover}
-                  />
+                    resizeMode="cover"
+                  >
+                    {myButton}
+                  </ImageBackground>
                 </View>
               </View>
             </Pressable>
@@ -49,10 +77,23 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
   },
-
-  imageCover: {
+  imageContainer: {
     width: 300,
     height: 200,
-    borderRadius:20,
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+  imageCover: {
+    // Make the image expand to cover the video's dimensions
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  playButton: {
+    backgroundColor: "grey",
+    borderRadius: 40,
+    padding: 15,
   },
 });
