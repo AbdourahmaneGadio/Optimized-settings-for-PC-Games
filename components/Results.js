c476461ae2c71955f711e99dffc25b996995114c
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2023-09-09 10:40:00 +0200

    :new: Add flat list for results

--- components/Results.js
+++ components/Results.js
@@ -1,19 +1,46 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
 import ZoneJeuIndividuel from "./ZoneJeuIndividuel";
 
-const Results = ({ gameResults, onPress}) => {
-
+const Results = ({ gameResults, onPress }) => {
   return (
     <View>
       <Text>Results : </Text>
-      {gameResults &&
-        gameResults.map &&
-        gameResults.map((actualGame, index) => {
-          return <ZoneJeuIndividuel actualGame={actualGame} key={index} onPress={onPress}/>;
-        })}
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={gameResults}
+          renderItem={({ item }) => (
+            <ZoneJeuIndividuel 
+            actualGame={item} 
+            onPress={onPress} />
+          )}
+        />
+      </SafeAreaView>
     </View>
   );
 };
 
 export default Results;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
