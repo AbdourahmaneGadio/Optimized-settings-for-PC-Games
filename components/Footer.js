2f3b8a2b9bccf41743510c3cda9044e2a6708530
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2023-09-18 20:20:58 +0200

    :grey_exclamation: Fix footer links

--- components/Footer.js
+++ components/Footer.js
@@ -7,20 +7,53 @@ export default function Footer() {
       style={{
         alignItems: "center",
         maxWidth: "70%",
-        maxHeight:"20%"
+        maxHeight: "20%",
       }}
     >
-      <Text
-        onPress={() => Linking.openURL("https://github.com/AbdourahmaneGadio/")}
-      >
-        © Copyright 2023 - GADIO Abdourahmane
-      </Text>
-      <Text onPress={() => Linking.openURL("https://rawg.io/")}>
-        Games's data provided by the RAWG API
+      <Text>
+        © Copyright 2023 -{" "}
+        <Text
+          style={{ color: "blue" }}
+          onPress={() =>
+            Linking.openURL("https://github.com/AbdourahmaneGadio/")
+          }
+        >
+          GADIO Abdourahmane
+        </Text>
       </Text>
       <Text>
-        Games's settings provided by multiples sources like Hardware Unboxed,
-        BenchmarKing, ...
+        Games's data provided by the{" "}
+        <Text
+          style={{ color: "blue" }}
+          onPress={() => Linking.openURL("https://rawg.io/apidocs")}
+        >
+          RAWG API
+        </Text>
+      </Text>
+      <Text>
+        Games's settings provided by multiples sources like{" "}
+        <Text
+          style={{ color: "blue" }}
+          onPress={() =>
+            Linking.openURL(
+              "https://www.youtube.com/channel/UCI8iQa1hv7oV_Z8D35vVuSg"
+            )
+          }
+        >
+          Hardware Unboxed
+        </Text>
+        ,{" "}
+        <Text
+          style={{ color: "blue" }}
+          onPress={() =>
+            Linking.openURL(
+              "https://www.youtube.com/channel/UCEL03vxCWzPzXcarexAIFHg"
+            )
+          }
+        >
+          BenchmarKing
+        </Text>
+        , ...
       </Text>
     </View>
   );
